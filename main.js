eb5d47ae8cb388fc6213c540015b3c26c23c3721
noctist <zohranlakdawala@hotmail.com>
2019-03-17 14:15:30 -0400

    finished product

--- main.js
+++ main.js
@@ -23,3 +23,29 @@
 //    If the user clicks "Add New" and the input is empty, 
 //    the new item's title needs to default to Title {number} (ex: Title 6 or Title 7)
 //    depending on the number of items already on the page
+ 
+$(document).ready(function() {
+
+	$("#add_new").click(function() {
+		let titleCount = $('.item').length;
+		if ($('#newtitle').val()) {
+			$(".item-list").append(`<div class="item"><h1 class="title">${$('#newtitle').val()}</h1><span class=close>x</span><p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id scelerisque lacus, rutrum lobortis nisl. Ut dapibus, quam in auctor pharetra, nibh erat pharetra velit, vitae tristique mauris dui quis.</p></div>`);
+			$("#newtitle").val('');
+		}
+		else {
+			$(".item-list").append(`<div class="item"><h1 class="title">Title ${titleCount+1}</h1><span class=close>x</span><p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id scelerisque lacus, rutrum lobortis nisl. Ut dapibus, quam in auctor pharetra, nibh erat pharetra velit, vitae tristique mauris dui quis.</p></div>`);
+		}
+	});
+
+	$(document).on('click', '.close', function() {
+		$(this).closest(".item").remove();
+	});
+	$(document).on('click', '.title', function() {
+		$(this).siblings(".description").slideToggle('slow')
+    
+	});
+
+
+})
+
+
